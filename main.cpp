616f87ff51c1d85df7f1ed846d91466d745396af
jhguzman <jordan.guzman09@gmail.com>
2016-09-21 10:45:52 -0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,20 @@
+//
+//  main.cpp
+//  Lab 4
+//
+//  Created by Jordan Guzman on 9/21/16.
+//  Copyright (c) 2016 Jordan Guzman. All rights reserved.
+//
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+int guess;
+int randNum = 100 + (rand() % 899);
+
+
+int main() {
+    return 0;
+}
