cfe46317f97da8d50f82d39fc3acae367b84bc70
jhguzman <jordan.guzman09@gmail.com>
2016-09-22 01:53:19 -0700

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -10,6 +10,7 @@
 #include <string>
 #include <ctime>
 #include <cstdlib>
+#include <vector>
 using namespace std;
 
 
@@ -24,25 +25,30 @@ public:
     void printResults();
 };
 
-
+//Write array to hold the three colors so it can check properly.
 void GuessVerification::guessVerifier(int guess){ //Needs work.
     int n = 100;
     randNum = 100 + (rand() % 899);
+    vector <int> colors(3);
     for (int i = 0; i < 3; i++){
         if (((guess/n) % 10) == ((randNum/100) % 10)){
             n = n/10;
-            green++;
+            //element at 0 ++
+            //swap
         } else if (((guess/n) % 10) == ((randNum/10) % 10)) {
             n = n/10;
-            yellow++;
+            //element at 1 ++
+            //swap
         } else if (((guess/n) % 10) == (randNum % 10)) {
             n = n/10;
-            yellow++;
+            //element at 2 ++
+            //swap
         } else {
             n = n/10;
             red++;
         }
-    } 
+    }
+    green = colors[2];
 }
 
 
@@ -57,7 +63,7 @@ void GuessVerification::printResults(){ //WORKS
 int main() {
     int guess;
     GuessVerification guessedValue;
-    srand(static_cast<unsigned int>(time(0)));
+    //srand(static_cast<unsigned int>(time(0)));
     cout<<"Try to guess the 3 digit number."<<endl;
     cin>>guess;
     guessedValue.guessVerifier(guess);
