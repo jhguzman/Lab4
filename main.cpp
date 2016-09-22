c71b156d16ad5a99d3721c7a0eb4d1caa2b562ef
jhguzman <jordan.guzman09@gmail.com>
2016-09-21 23:29:58 -0700

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -13,14 +13,49 @@
 using namespace std;
 
 
-int guess = 145;
+class GuessVerification {
+    int guess;
+    int randNum;
+    int green;
+    int yellow;
+    int red;
+private:
+    void randomNumber();
+    void guessVerifier(int randNum, int guess);
+    void printResults();
+};
+
+void GuessVerification::randomNumber(){
+    randNum = 100 + (rand() % 899);
+}
+
+void GuessVerification::guessVerifier(int randNum, int guess){
+    int n = 100;
+    for (int i = 0; i < 3; i++){
+        if (((guess/n) % 10) == ((randNum/100) % 10)){
+            n = n/10;
+            green++;
+        } else if (((guess/n) % 10) == ((randNum/10) % 10)) {
+            n = n/10;
+            yellow++;
+        } else if (((guess/n) % 10) == (randNum % 10)) {
+            n = n/10;
+            yellow++;
+        } else {
+            n = n/10;
+            red++;
+        }
+    } 
+}
+
+void GuessVerification::printResults(){
+    
+}
+
+
+
 
 int main() {
-    //srand(static_cast<unsigned int>(time(0)));
-    //int randNum = 100 + (rand() % 899);
-    cout<<guess<<endl;
-    cout<<(guess/100) % 10<<endl; //Gives first digit
-    cout<<(guess/10) % 10<<endl; //Gives second digit
-    cout<<guess % 10<<endl; //Gives third digit
+    srand(static_cast<unsigned int>(time(0)));
     return 0;
 }
