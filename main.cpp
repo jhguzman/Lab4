13156b96bd586beb5604955ea2d7d41cf7fdb977
jhguzman <jordan.guzman09@gmail.com>
2016-09-22 21:43:29 -0700

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -10,7 +10,6 @@
 #include <string>
 #include <ctime>
 #include <cstdlib>
-#include <vector>
 using namespace std;
 
 
@@ -25,27 +24,22 @@ public:
     void printResults();
 };
 
-//Write array to hold the three colors so it can check properly.
-void GuessVerification::guessVerifier(int guess){ //Needs work.
+
+void GuessVerification::guessVerifier(int guess){ //WORKS
     int n = 100;
     randNum = 100 + (rand() % 899);
-    vector <int> colors(3);
     
     for (int i = 1; i <= 3; i++){
         if (i == 1){
             if (((guess/n) % 10) == ((randNum/100) % 10)){ //compares guess to randNum digit 1
                 n = n/10;
-                //colors.at(j) = colors.at(j) + 1; //Updates element.
-                //element at 0 ++
-                //swap
+                green++;
             } else if (((guess/n) % 10) == ((randNum/10) % 10)) { //compares guess to randNum digit 2
                 n = n/10;
-                //element at 1 ++
-                //swap
+                yellow++;
             } else if (((guess/n) % 10) == (randNum % 10)) { //compares guess to randNum digit 3
                 n = n/10;
-                //element at 2 ++
-                //swap
+                yellow++;
             } else {
                 n = n/10;
                 red++;
@@ -54,36 +48,28 @@ void GuessVerification::guessVerifier(int guess){ //Needs work.
         } else if (i == 2){
             if (((guess/n) % 10) == ((randNum/10) % 10)){ //compares guess to randNum digit 1
                 n = n/10;
-                //colors.at(j) = colors.at(j) + 1; //Updates element.
-                //element at 0 ++
-                //swap
+                green++;
             } else if (((guess/n) % 10) == ((randNum/100) % 10)) { //compares guess to randNum digit 2
                 n = n/10;
-                //element at 1 ++
-                //swap
+                yellow++;
             } else if (((guess/n) % 10) == (randNum % 10)) { //compares guess to randNum digit 3
                 n = n/10;
-                //element at 2 ++
-                //swap
+                yellow++;
             } else {
                 n = n/10;
                 red++;
             }
-
+            
         } else if (i == 3){
             if (((guess/n) % 10) == (randNum % 10)){ //compares guess to randNum digit 1
                 n = n/10;
-                //colors.at(j) = colors.at(j) + 1; //Updates element.
-                //element at 0 ++
-                //swap
+                green++;
             } else if (((guess/n) % 10) == ((randNum/100) % 10)) { //compares guess to randNum digit 2
                 n = n/10;
-                //element at 1 ++
-                //swap
+                yellow++;
             } else if (((guess/n) % 10) == ((randNum/10) % 10)) { //compares guess to randNum digit 3
                 n = n/10;
-                //element at 2 ++
-                //swap
+                yellow++;
             } else {
                 n = n/10;
                 red++;
@@ -92,8 +78,6 @@ void GuessVerification::guessVerifier(int guess){ //Needs work.
         }
         
     }
-    //green = colors.at(2);
-    //yellow = colors.at(1) + colors.at(0);
 }
 
 
@@ -106,25 +90,12 @@ void GuessVerification::printResults(){ //WORKS
 
 
 int main() {
-    //int guess;
-    //GuessVerification guessedValue;
-    //srand(static_cast<unsigned int>(time(0)));
-    /* cout<<"Try to guess the 3 digit number."<<endl;
-     cin>>guess;
-     guessedValue.guessVerifier(guess);
-     guessedValue.printResults();
-     */
-    vector <int> test(3);
-    test.at(0) = 1;
-    test.at(1) = 45;
-    test.at(2) = 209;
-    cout<<test.at(0)<<endl;
-    test.at(0) = test.at(0) + 1;
-    cout<<test.at(0)<<endl;
-    
-    
-    
-    
-    
+    int guess;
+    GuessVerification guessedValue;
+    srand(static_cast<unsigned int>(time(0)));
+    cout<<"Try to guess the 3 digit number."<<endl;
+    cin>>guess;
+    guessedValue.guessVerifier(guess);
+    guessedValue.printResults();
     return 0;
 }
