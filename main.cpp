c3dfa8ec69ec7b8dc5aeffbc8f1a7447e43f30ef
jhguzman <jordan.guzman09@gmail.com>
2016-09-23 19:13:26 -0700

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -15,20 +15,22 @@ using namespace std;
 
 class GuessVerification {
     int guess;
-    int randNum;
+    int randNum = 100 + (rand() % 899);
     int green;
     int yellow;
     int red;
+    
 public:
     void guessVerifier(int guess);
     void printResults();
 };
 
 
-void GuessVerification::guessVerifier(int guess){ //WORKS
+void GuessVerification::guessVerifier(int guess){
     int n = 100;
-    randNum = 100 + (rand() % 899);
-    
+    green = 0;
+    yellow = 0;
+    red = 0;
     for (int i = 1; i <= 3; i++){
         if (i == 1){
             if (((guess/n) % 10) == ((randNum/100) % 10)){ //compares guess to randNum digit 1
@@ -44,7 +46,7 @@ void GuessVerification::guessVerifier(int guess){ //WORKS
                 n = n/10;
                 red++;
             }
-
+            
         } else if (i == 2){
             if (((guess/n) % 10) == ((randNum/10) % 10)){ //compares guess to randNum digit 1
                 n = n/10;
@@ -74,28 +76,34 @@ void GuessVerification::guessVerifier(int guess){ //WORKS
                 n = n/10;
                 red++;
             }
-
+            
         }
         
     }
+    printResults();
 }
 
 
-void GuessVerification::printResults(){ //WORKS
+void GuessVerification::printResults(){
     cout<<"You have: "<<red<<" red"<<endl;
     cout<<"          "<<green<<" green"<<endl;
     cout<<"          "<<yellow<<" yellow"<<endl;
+    if (green != 3){
+        cout<<"Enter the next guess."<<endl;
+        int guessAgain;
+        cin>>guessAgain;
+        return guessVerifier(guessAgain);
+    }
     cout<<"The number was "<<randNum<<endl;
 }
 
 
 int main() {
     int guess;
-    GuessVerification guessedValue;
     srand(static_cast<unsigned int>(time(0)));
+    GuessVerification guessedValue;
     cout<<"Try to guess the 3 digit number."<<endl;
     cin>>guess;
     guessedValue.guessVerifier(guess);
-    guessedValue.printResults();
     return 0;
 }
